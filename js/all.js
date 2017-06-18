cecdc0da12e156f5aa869e2e3c057bdc7d281161
Odei Riveiro Zafra <odei.riveiro@gmail.com>
2017-06-18 11:23:26 +0200

    Asignados eventos y funcionalidades varias

---
+++ js/all.js
@@ -0,0 +1,19 @@
+window.onload = function() {
+    var numBotones = 9;
+    var tiempoPista = 3;
+    var botones = cargaEventos(numBotones * 2, "onclick", "button", "comprobarBoton(this.id)", "disabled", "cardBack");
+    document.getElementById('formulario').innerHTML = botones + document.getElementById('formulario').innerHTML;
+    document.getElementById("avatar").addEventListener("click", function(){asistente();});
+    document.getElementById("empezar").addEventListener("click", function(){comienzo();});
+    document.getElementById("finalizar").addEventListener("click", function(){finJuego();});
+    document.getElementById("ayuda").addEventListener("click", function(){ayuda();});
+    document.getElementById("record").addEventListener("click", function(){consultarRecord();});
+    inicializar(numBotones, tiempoPista * 1000);
+    function cargaEventos(n, evento, tipo, funcion, activa, clase) {
+        var aux = "";
+        for (var i = 0; i < n; i++) {
+            aux += "<input type='"+tipo+"' id='"+i+"' name='"+clase+"' class='"+clase+"' "+evento+"='"+funcion+"' "+activa+">\n";
+        }
+        return aux;
+    }
+};
