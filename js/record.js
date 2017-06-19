c95caadc4178f2e0a0f3854d8f6a4877590a2a69
Odei Riveiro Zafra <odei.riveiro@gmail.com>
2017-06-19 11:28:45 +0200

    Agregado función crearCookie

--- js/record.js
+++ js/record.js
@@ -1,3 +1,9 @@
 function borrarCookie(id){									//Función usada para "borrar" cookie almacenada solo accesible con el modo debug activado
 	document.cookie = id+'=; expires=Thu, 01 Jan 1970 00:00:01 GMT;';
 }
+function crearCookie(id,valor,caducidad){					//Función usada para crear cookie a partir de un id, valor y fecha de caducidad recibidas como parámetros
+	var d = new Date();
+	d.setTime(d.getTime() + (caducidad*24*60*60*1000));
+	var expires = "expires=" + d.toGMTString();
+	document.cookie = id+"=" + valor + "; " + expires;
+}
